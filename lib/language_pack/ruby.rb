9307f2b886a079658afd215a2b56a46a711cf406
Greg Benedict <gbenedict@tgfi.net>
2013-01-08 15:17:07 -0500

    debugging

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -403,7 +403,7 @@ ERROR
       # end
 
       Dir.mktmpdir("libyaml-") do |tmpdir|
-        
+
 
         libsqlite3_dir = "#{tmpdir}/#{LIBSQLITE3_PATH}"
         install_libsqlite3(libsqlite3_dir)
@@ -422,6 +422,7 @@ ERROR
         # we need to set BUNDLE_CONFIG and BUNDLE_GEMFILE for
         # codon since it uses bundler.
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:#{sqlite3_include}:$CPATH CPPATH=#{yaml_include}:#{sqlite3_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:#{sqlite3_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
+        raise env_vars
         puts "Running: #{bundle_command}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
 
