6b07181c43302fefe331ff8bda98600c172823b0
Terence Lee <hone02@gmail.com>
2013-07-16 13:05:04 -0400

    v68

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -10,7 +10,7 @@ class LanguagePack::Ruby < LanguagePack::Base
   extend LanguagePack::BundlerLockfile::ClassMethods
 
   NAME                 = "ruby"
-  BUILDPACK_VERSION    = "v67"
+  BUILDPACK_VERSION    = "v68"
   LIBYAML_VERSION      = "0.1.4"
   LIBYAML_PATH         = "libyaml-#{LIBYAML_VERSION}"
   LIBSQLITE3_PATH      = "fatstax-sqlite-vulcan-3071501"
